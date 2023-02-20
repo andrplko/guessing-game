241e14c9dba16b4208ac4ae6bed602e25e0c646f
Andrei Paleshka <andrepoleshko@gmail.com>
2023-02-20 08:42:35 +0100

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        this.assumption = Math.round(this.maxValue - ((this.maxValue - this.minValue) / 2));
+        return this.assumption;
     }
 
     lower() {
-
+        this.maxValue = this.assumption;
     }
 
     greater() {
-
+        this.minValue = this.assumption;
     }
 }
 
